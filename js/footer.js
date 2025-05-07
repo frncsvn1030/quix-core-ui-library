b7b3cae87ea6fa54b83d4911378f317918fe6607
Francis_DEV <f.vino@qstrike.com>
2025-05-08 03:25:13 +0800

    docs: create navigation layout & refactor navbar/footer into reusable partials

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+    fetch('/partials/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading navbar:', error);
+        });
+});
+
---
+++ js/navbarDocs.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+    fetch('/partials/navbarDocs.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading navbar:', error);
+        });
+});
